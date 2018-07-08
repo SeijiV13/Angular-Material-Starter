4c22126ede0a3377340cfc0d03d4a0eaf6457053
seiji <villafrancasra@gmail.com>
2018-07-08 12:20:50 +0800

    initiliaze angular material code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule, BaseRequestOptions, RequestOptions, Http } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
+import { ROUTER_PROVIDER } from './routes/main.routes';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ROUTER_PROVIDER,
+    ReactiveFormsModule,
+    HttpModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes/main.routes.ts
@@ -0,0 +1,15 @@
+
+import { Routes, RouterModule } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+
+
+//PAGE COMPONENTS
+
+
+export const routes: Routes = [
+   
+ 
+
+];
+
+export const ROUTER_PROVIDER: ModuleWithProviders = RouterModule.forRoot(routes);
